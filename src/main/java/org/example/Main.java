2728bae6396629d6727ba9aa21c8ff1778b2f7d2
Sankalp Boolchandani <boolchandanisankalp@gmail.com>
2025-07-05 12:57:32 +0530

    breaking singleton using constructor

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,7 +1,25 @@
 package org.example;
 
+import singleton.EnumSingleton;
+import singleton.LazySingleton;
+
+import java.lang.reflect.Constructor;
+import java.lang.reflect.InvocationTargetException;
+
 public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello, World!");
+    public static void main(String[] args) throws InvocationTargetException, InstantiationException, IllegalAccessException {
+//        System.out.println("Hello, World!");
+
+//        EnumSingleton instance = EnumSingleton.INSTANCE;
+//        instance.hashCode();
+
+        LazySingleton instance1 = LazySingleton.getInstance();
+        Constructor<?>[] declaredConstructors = LazySingleton.class.getDeclaredConstructors();
+        declaredConstructors[0].setAccessible(true);
+        Constructor<LazySingleton> declaredConstructor = (Constructor<LazySingleton>) declaredConstructors[0];
+        LazySingleton instance2 = declaredConstructor.newInstance();
+        System.out.println(instance1.hashCode());
+        System.out.println(instance2.hashCode());
+
     }
 }
