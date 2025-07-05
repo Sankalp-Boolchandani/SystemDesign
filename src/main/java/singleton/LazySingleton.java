15371c16c1103a860de804a9b2fc72eb1c2946de
Sankalp Boolchandani <boolchandanisankalp@gmail.com>
2025-07-05 13:03:46 +0530

    solution to singleton breaking using constructor

--- src/main/java/singleton/LazySingleton.java
+++ src/main/java/singleton/LazySingleton.java
@@ -4,9 +4,13 @@ public class LazySingleton {
 
     private static LazySingleton singletonDemo=null;
 
-    private LazySingleton(){}
+    private LazySingleton() throws Exception {
+        if (singletonDemo!=null){
+            throw new Exception("object already exists");
+        }
+    }
 
-    public static LazySingleton getInstance(){
+    public static LazySingleton getInstance() throws Exception {
         if (singletonDemo==null){
             singletonDemo=new LazySingleton();
         }
