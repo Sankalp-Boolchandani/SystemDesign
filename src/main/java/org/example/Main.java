b277243818eabc659d4eefae855a7e4f9a6149ac
Sankalp Boolchandani <boolchandanisankalp@gmail.com>
2025-07-05 12:58:13 +0530

    Enum Singleton

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -10,16 +10,16 @@ public class Main {
     public static void main(String[] args) throws InvocationTargetException, InstantiationException, IllegalAccessException {
 //        System.out.println("Hello, World!");
 
-//        EnumSingleton instance = EnumSingleton.INSTANCE;
-//        instance.hashCode();
+        EnumSingleton instance = EnumSingleton.INSTANCE;
+        instance.hashCode();
 
-        LazySingleton instance1 = LazySingleton.getInstance();
-        Constructor<?>[] declaredConstructors = LazySingleton.class.getDeclaredConstructors();
-        declaredConstructors[0].setAccessible(true);
-        Constructor<LazySingleton> declaredConstructor = (Constructor<LazySingleton>) declaredConstructors[0];
-        LazySingleton instance2 = declaredConstructor.newInstance();
-        System.out.println(instance1.hashCode());
-        System.out.println(instance2.hashCode());
+//        LazySingleton instance1 = LazySingleton.getInstance();
+//        Constructor<?>[] declaredConstructors = LazySingleton.class.getDeclaredConstructors();
+//        declaredConstructors[0].setAccessible(true);
+//        Constructor<LazySingleton> declaredConstructor = (Constructor<LazySingleton>) declaredConstructors[0];
+//        LazySingleton instance2 = declaredConstructor.newInstance();
+//        System.out.println(instance1.hashCode());
+//        System.out.println(instance2.hashCode());
 
     }
 }
